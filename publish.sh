fb9530d70f3f870b94c4acb7f751150b65f3b1fe
Lito Julius Goldmann <l.goldmann@campus.tu-berlin.de>
2019-06-11 17:41:48 +0200

    Add publish script

---
+++ publish.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]
+then
+    echo "Usage ./publish. <version>"
+    exit 1
+fi
+
+if [ ! -f ./VERSION ]
+then
+    echo "Wrong folder!!"
+    exit 1
+fi
+
+set -x 
+VERSION=$1
+
+echo "$1" > VERSION
+
+git add VERSION
+git commit -m "Bump version to $VERSION"
+git push
+
+git tag -a "$VERSION" -m "$VERSION"
+
+git push origin "$VERSION"
